54b84fc36347b510ea9c2a28ec06da029e8cd7b2
Yash <raunakyash12@gmail.com>
2026-02-24 19:25:00 +0530

    fixed bugs

--- stash/commands/upload.py
+++ stash/commands/upload.py
@@ -10,14 +10,16 @@ import pyperclip
 import math
 from rich.table import Table
 
+console = Console()
+
 def createMetadataList(files):
     metadataList = []
 
     for f in files:
         file = Path(f)
-        # if not Path(f.is_file()):
-        #     print(f"invalid file type {file}")
-        #     continue
+        if not file.is_file():
+            print(f"[red] invalid file type {file}")
+            continue
         size = file.stat().st_size 
         formattedSize = getSize(size)
         data = {
@@ -62,7 +64,6 @@ def upload(files:List[str],informative:bool,copyToClipboard:bool):
 
     BACKEND_URL = "https://stashit-uqpt.onrender.com"
     serverUrl = f"{BACKEND_URL}/api/file/upload"
-    testfileurl = "cli/utils/img.png"
     print(files)
     metadataList = createMetadataList(files)
 
@@ -72,8 +73,7 @@ def upload(files:List[str],informative:bool,copyToClipboard:bool):
             "files": filteredMetadata,
             "expiry": "once"
             }
-    print("-> registering files on database \n")
-    console = Console()
+    print("\n[green]-> registering files on database \n")
     with console.status("Processing..",spinner="dots") as status:
         response = requests.post(serverUrl,json = payload)
         res = json.loads(response.text)
@@ -81,26 +81,27 @@ def upload(files:List[str],informative:bool,copyToClipboard:bool):
     stashKey = res["stashKey"]
     try:
         for url in signedUrls:
-            for data in metadataList:
-                if data["fileInfo"]["id"]== url["id"]:
-                    path = data["filePath"]
-            # if informative :
-            #     inform = input(f"upload file y/yes ")
-            #     if not (inform == 'y' or inform.lower() == "yes" or inform == " "):
-            #         continue
-            with console.status(" Processing..",spinner="dots") as status:
-                with open(path, "rb" ) as f:
-                    print(f"-> {data["fileInfo"]["name"]} uploading... ")
-                    if f:
-                        response = requests.put(url["uploadUrl"]
-                                            ,data = f,
-                                            headers= {"Content-Type" :
-                                                "application/octet-stream"
-                                                }
-                                        )
-                    
-            print(f"-> {data["fileInfo"]["name"]} sucessfully uploaded")
-
+            found = False
+            for metadata in metadataList:
+                if metadata["fileInfo"]["id"]== url["id"]:
+                    data = metadata
+                    found = True
+            if found :
+                # if informative :
+                #     inform = input(f"upload file y/yes ")
+                #     if not (inform == 'y' or inform.lower() == "yes" or inform == " "):
+                #         continue
+                with console.status(f" Processing.. {url["name"]} size : {data["fileInfo"]["formattedSize"]["size"]}",spinner="dots") as status:
+                    with open(data["filePath"], "rb" ) as f:
+                        if f:
+                            response = requests.put(url["uploadUrl"]
+                                                ,data = f,
+                                                headers= {"Content-Type" :
+                                                    "application/octet-stream"
+                                                    }
+                                            )
+                        
+                print(f"[green]-> {url["name"]} sucessfully uploaded")
 
     except FileNotFoundError:
         print("error file not found")
@@ -112,12 +113,12 @@ def upload(files:List[str],informative:bool,copyToClipboard:bool):
             pyperclip.copy(stashKey)
             print("\n-> copied stash key to clipboard")
         except:
-            print("failed copying to clipboard")
+            print("[red]failed copying to clipboard")
 
 
 
     print("\n----------------------------------------------------------------------\n")
-    console.print(f"\t[bold blue] {stashKey}")
+    print(f"\t[bold blue] {stashKey}")
     print("\n----------------------------------------------------------------------\n")
     
     # table = Table(title="Upload Summary")
--- stash/main.py
+++ stash/main.py
@@ -1,23 +1,35 @@
 import typer
 from rich import print
+from rich.console import Console
+from rich.text import Text
 from stash.commands.upload import upload
 from stash.commands.download import download
 from typing import List
 
 app = typer.Typer(invoke_without_command=True)
+console = Console()
 
 @app.callback(invoke_without_command=True)
 def main(
-    target: List[str] = typer.Argument(...),
+    ctx:typer.Context,
+    target: List[str] = typer.Argument(None),
     download_mode:bool=typer.Option(False,"-key","-k"),
     informative:bool=typer.Option(False,"-i","-informative"),
     copy_to_clipboard :bool =typer.Option(False,"-c","-copy")
 ):
-    print(download_mode)
-    if not download_mode:
-        upload(target,informative,copy_to_clipboard)
-    else:
-        download(target[0])
+    if ctx.invoked_subcommand is None:
+        if not target:
+            text = Text("STASH", style="bold cyan")
+            text.stylize("bold magenta", 0, 2)
+            print(text)
+            print("Upload files on temporary storage and download them through key")
+            print("\nUse --help to see available commands.\n")
+            print(":: [white]stash <file_name/source>[/white] \n")
+            print("Use -k or -key to download files \n:: stash [green]-k <stash-key>\n")
+        elif not download_mode:
+            upload(target,informative,copy_to_clipboard)
+        else:
+            download(target[0])
 
 if __name__ == "__main__":
     app()
