f7648f8d96cf887bc294311220342bfcaf3920a0
Yash <raunakyash12@gmail.com>
2026-02-26 23:26:30 +0530

    added config updation logic

---
+++ stash/config.py
@@ -0,0 +1,57 @@
+import os
+import json
+
+defaultConfig = {
+    "BACKEND_URL":"https://stashit-uqpt.onrender.com",
+    "DOWNLOAD_DIR":"."#current directory
+}
+dir_name = "stash"
+file_name = "config.json"
+file_path = os.path.join(dir_name,file_name)
+
+def ensureConfig():
+    try:
+        os.makedirs(dir_name,exist_ok=True)
+        if not os.path.exists(file_path):
+            with open(file_path,"w") as f:
+                f.write(json.dumps(defaultConfig))
+            print(file_path)
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+    except PermissionError:
+        print(f"Error: No permission to read '{file_path}'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    
+def loadConfig():
+    try:
+        ensureConfig()
+        with open(file_path , "r") as f:
+            data = f.read()
+            print(json.loads(data)["BACKEND_URL"])
+            return json.loads(data)
+
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+    except PermissionError:
+        print(f"Error: No permission to read '{file_path}'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def updateConfig(config_key,config_value):
+    try:
+        ensureConfig()
+        config = loadConfig()
+        with open(file_path,"w") as f:
+            config[config_key] = config_value
+            f.write(json.dumps(config))
+
+    except FileNotFoundError:
+        print(f"Error: File '{file_path}' not found.")
+    except PermissionError:
+        print(f"Error: No permission to read '{file_path}'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
